16d7adaa069dc960154bbd21f464c54b5599a6b1
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-10-28 08:13:25 +0530

    Update FirstName.java

--- RegularExpression/FirstName.java
+++ RegularExpression/FirstName.java
@@ -2,7 +2,7 @@ package RegularExpression;
 
 import java.util.regex.Pattern;
 
-public class FirstName {
+public class UserDetails {
 	public static void main(String[] args) {
 		final String REGEX_EMAIL = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 		System.out.println(Pattern.matches(REGEX_EMAIL, "abc@yahoo.com"));
