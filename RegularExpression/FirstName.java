97d6d3b7b000f4804d591a1c1154cd794569cc73
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-10-28 08:10:10 +0530

    Update FirstName.java

--- RegularExpression/FirstName.java
+++ RegularExpression/FirstName.java
@@ -3,7 +3,7 @@ package RegularExpression;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class FirstName {
+public class UserDetails {
 	public String inputMethod() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter String");
@@ -11,7 +11,7 @@ public class FirstName {
 		return string;
 	}
 	public static void main(String[] args) {
-		FirstName user = new FirstName();
+		UserDetails user = new UserDetails();
 		final String REGEX_FIRSTNAME = "([A-Z][a-zA-Z]{2,})";
 		final String REGEX_LASTNAME = "([A-Z][a-zA-Z]{2,})";
 		System.out.println("Validation for First name :" + Pattern.matches(REGEX_FIRSTNAME, user.inputMethod()));
