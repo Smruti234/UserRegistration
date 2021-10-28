521ba7f2710eb760d5a6791a43c94e13bbecb10c
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-10-28 08:09:15 +0530

    Update FirstName.java

--- RegularExpression/FirstName.java
+++ RegularExpression/FirstName.java
@@ -1,5 +1,22 @@
 package RegularExpression;
 
-public class FirstName {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class UserDetails {
+	public String inputMethod() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter String");
+		String string = scanner.nextLine();
+		return string;
+	}
+
+	public static void main(String[] args) {
+		UserDetails user = new UserDetails();
+		final String REGEX_FIRSTNAME = "([A-Z][a-zA-Z]{2,})";
+		Pattern fName = Pattern.compile(REGEX_FIRSTNAME);
+		Matcher matchForFName = fName.matcher(user.inputMethod());
+		System.out.println("Your name is Valid : " + matchForFName.matches());
+	}
 }
